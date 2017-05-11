1194bd4cb1d32cbe08d555eeb33dcca82e8963c9
Erik Tauchert <erik@tauchert.de>
2017-05-11 14:22:26 +0200

    use bilinear filtering on shadow map sampling

--- App1/ECS/Components/LightComponent.h
+++ App1/ECS/Components/LightComponent.h
@@ -23,8 +23,10 @@ namespace ECS { namespace Components {
 
 				glBindTexture(GL_TEXTURE_2D, this->shadowMapTextureId);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, this->shadowMapResolution, this->shadowMapResolution, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 				GLfloat borderColor[] = { 1.f, 1.f, 1.f, 1.f };
