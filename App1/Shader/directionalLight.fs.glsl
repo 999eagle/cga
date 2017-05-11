8f98f6dca33a7ed34967b118b7d61e8abcf668cd
Erik Tauchert <erik@tauchert.de>
2017-05-11 10:51:36 +0200

    implement 16 sample pcf for shadow map

--- App1/Shader/directionalLight.fs.glsl
+++ App1/Shader/directionalLight.fs.glsl
@@ -100,6 +100,29 @@ vec3 BRDF_CookTorrance(Fragment fragment, vec3 viewDir, vec3 lightDir)
 	return ((vec3(1.0) - F) * (1.0 - fragment.metallic) * diffuse + specular) * ndl;
 }
 
+float GetShadowMapValue(Fragment fragment, vec3 lightDir)
+{
+	float shadow = 0.0;
+	if (enableShadowMap)
+	{
+		vec2 texelSize = 1.0 / textureSize(shadowMapTexture, 0);
+		vec4 lightFragment = lightViewProj * vec4(fragment.worldPos, 1.0);
+		lightFragment /= lightFragment.w;
+		lightFragment.xyz = lightFragment.xyz * 0.5 + 0.5;
+		float bias = max(0.05 * (1.0 - dot(fragment.normal, lightDir)), 0.005);
+		for (float x = -1.5; x <= 1.5; x++)
+		{
+			for (float y = -1.5; y <= 1.5; y++)
+			{
+				float lightDepth = texture2D(shadowMapTexture, lightFragment.xy + vec2(x, y) * texelSize).r;
+				shadow += (lightDepth < lightFragment.z - bias) ? 1.0 : 0.0;
+			}
+		}
+		shadow /= 16.0;
+	}
+	return 1.0 - shadow;
+}
+
 void main()
 {
 	Fragment fragment = ReadGBuffer();
@@ -107,22 +130,9 @@ void main()
 	vec3 viewDir = normalize(cameraPosition - fragment.worldPos);
 	vec3 lightDir = normalize(-lightDirection);
 
-	if (enableShadowMap)
-	{
-		vec4 lightFragment = lightViewProj * vec4(fragment.worldPos, 1.0);
-		lightFragment /= lightFragment.w;
-		lightFragment.xyz = lightFragment.xyz * 0.5 + 0.5;
-		float lightDepth = texture(shadowMapTexture, lightFragment.xy).r;
-		float bias = max(0.05 * (1.0 - dot(fragment.normal, lightDir)), 0.005);
-		if (lightFragment.z - bias > lightDepth)
-		{
-			discard;
-		}
-	}
+	vec3 light = BRDF_CookTorrance(fragment, viewDir, lightDir) * GetShadowMapValue(fragment, lightDir);
 
-	vec3 brdf = BRDF_CookTorrance(fragment, viewDir, lightDir);
+	light *= lightColor;
 
-	brdf *= lightColor;
-
-	outColor = vec4(brdf, 1.0);
+	outColor = vec4(light, 1.0);
 }
