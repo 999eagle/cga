b0134e72e592651246bcfe2f23a7a994adf063c4
Erik Tauchert <erik@tauchert.de>
2017-05-24 12:35:27 +0200

    improve bloom quality and performance
    bloom now uses downsampled versions of the scene for blurring

--- App1/PostProcessing/Bloom.h
+++ App1/PostProcessing/Bloom.h
@@ -11,13 +11,21 @@ class BloomPostProcessing
 public:
 	BloomPostProcessing(GLint width, GLint height)
 	{
+		this->width = width;
+		this->height = height;
 		this->brightnessShader = std::make_unique<Shader>("Shader\\passthrough.vs.glsl", "Shader\\postBloomBrightness.fs.glsl");
 		this->bloomShader = std::make_unique<Shader>("Shader\\passthrough.vs.glsl", "Shader\\postBloomRender.fs.glsl");
-		this->blurRenderer = new BlurRenderer(width, height);
+		this->blurRenderer[0] = new BlurRenderer(width, height);
+		this->blurRenderer[1] = new BlurRenderer(width / 2, height / 2);
+		this->blurRenderer[2] = new BlurRenderer(width / 4, height / 4);
+		this->blurRenderer[3] = new BlurRenderer(width / 8, height / 8);
 	}
 	~BloomPostProcessing()
 	{
-		delete this->blurRenderer;
+		delete this->blurRenderer[0];
+		delete this->blurRenderer[1];
+		delete this->blurRenderer[2];
+		delete this->blurRenderer[3];
 	}
 	void Draw(PostProcessing * postProcessing)
 	{
@@ -29,20 +37,35 @@ public:
 		// Pass 2: blur postProcessing FB 1 into blurRenderer FB 3 times
 		postProcessing->Swap(false);
 		postProcessing->BindReadTexture();
-		this->blurRenderer->BlurCurrentlyBoundTexture(1, 4);
-		this->blurRenderer->BlurCurrentlyBoundTexture(2, 3);
+		this->blurRenderer[0]->BlurCurrentlyBoundTexture(2);
+		postProcessing->BindReadTexture();
+		this->blurRenderer[1]->BlurCurrentlyBoundTexture(2);
+		postProcessing->BindReadTexture();
+		this->blurRenderer[2]->BlurCurrentlyBoundTexture(2);
+		postProcessing->BindReadTexture();
+		this->blurRenderer[3]->BlurCurrentlyBoundTexture(2);
 		// Pass 3: apply bloom effect
-		// pass 2 didn't overwrite postProcessing FB 0, so we still have the completely unprocessed texture available in that buffer
-		postProcessing->Swap();
+		// pass 2 didn't overwrite postProcessing FB 0, so we still have the completely unprocessed texture available in that buffer --> blend bloom additively
+		postProcessing->BindFramebuffer();
+		glViewport(0, 0, width, height);
+		glEnable(GL_BLEND);
+		glBlendEquation(GL_FUNC_ADD);
+		glBlendFunc(GL_ONE, GL_ONE);
 		// bind  blurred texture to texture 1, so original texture and blurred brightness texture are available to bloom shader
-		this->blurRenderer->BindBlurredTexture(1);
 		this->bloomShader->Apply();
 		glUniform1i(this->bloomShader->GetUniformLocation("colorTexture"), 0);
-		glUniform1i(this->bloomShader->GetUniformLocation("bloomTexture"), 1);
+		this->blurRenderer[0]->BindBlurredTexture();
 		QuadRenderer::GetInstance().DrawFullscreenQuad();
-
+		this->blurRenderer[1]->BindBlurredTexture();
+		QuadRenderer::GetInstance().DrawFullscreenQuad();
+		this->blurRenderer[2]->BindBlurredTexture();
+		QuadRenderer::GetInstance().DrawFullscreenQuad();
+		this->blurRenderer[3]->BindBlurredTexture();
+		QuadRenderer::GetInstance().DrawFullscreenQuad();
+		glDisable(GL_BLEND);
 	}
 private:
 	std::shared_ptr<Shader> brightnessShader, bloomShader;
-	BlurRenderer * blurRenderer;
+	BlurRenderer *blurRenderer[4];
+	GLsizei width, height;
 };
--- App1/Shader/gaussBlur.fs.glsl
+++ App1/Shader/gaussBlur.fs.glsl
@@ -2,7 +2,7 @@
 
 uniform sampler2D colorTexture;
 uniform bool horizontal;
-uniform int mipmapSamples;
+uniform vec2 viewportSize;
 
 in vec2 texCoord;
 out vec4 outColor;
@@ -15,27 +15,23 @@ uniform float weights[3] = float[]( 0.2270270270, 0.3162162162, 0.0702702703 );
 
 void main()
 {
-	vec2 texelSize = 1.0 / textureSize(colorTexture, 0);
-	outColor = vec4(0.0);
-	for (int m = 0; m < mipmapSamples; m++)
+	vec2 texelSize = 1.0 / viewportSize;
+
+	outColor.rgb = texture(colorTexture, texCoord).rgb * weights[0];
+	for (int i = 1; i < 3; i++)
 	{
-		outColor.rgb += textureLod(colorTexture, texCoord, m).rgb * weights[0];
-		for (int i = 1; i < 3; i++)
+		vec2 offset = vec2(0.0);
+		if (horizontal)
 		{
-			vec2 offset = vec2(0.0);
-			if (horizontal)
-			{
-				offset.x = offsets[i];
-			}
-			else
-			{
-				offset.y = offsets[i];
-			}
-			offset *= texelSize;
-			outColor.rgb += textureLod(colorTexture, texCoord + offset, m).rgb * weights[i];
-			outColor.rgb += textureLod(colorTexture, texCoord - offset, m).rgb * weights[i];
+			offset.x = offsets[i];
 		}
+		else
+		{
+			offset.y = offsets[i];
+		}
+		offset *= texelSize;
+		outColor.rgb += texture(colorTexture, texCoord + offset).rgb * weights[i];
+		outColor.rgb += texture(colorTexture, texCoord - offset).rgb * weights[i];
 	}
-	outColor.rgb /= vec3(mipmapSamples);
 	outColor.a = 1.0;
 }
